a73f46e7e86fba4b60728ac84545b6e9b647578b
cocastic8590 <cocasticmustdo@gmail.com>
2021-07-22 12:26:52 -0700

    add comments

--- VHDL_Code/basic_clocked_circuits.vhd
+++ VHDL_Code/basic_clocked_circuits.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+
+-------Entity Definition made here-----------
 entity Clocked_process_one is
   port (
     clock, reset, sig, a, b : in std_logic;
@@ -11,17 +13,28 @@ end Clocked_process_one;
 ----------------
 
 architecture Behavioural of Clocked_process_one is
-
+-----------Normally signals and constants come here too
     signal sig_x : std_logic;
 begin
+-------And variables also belong this guy
 
+    ----Sensitivity list taking all signals that has the possible of changing values
     process_x : process( clock, a, b, reset, sig )
     
     begin
 	 sig_x <= sig;
+
+
+     -----Asynchronous reset clocked process.
+     ----When the reset goes high it doesnt matter what clock says the assignment
+     ---- within the reset block must be executed 
         if reset ='1' then
             out_x <='0';
-     
+     ------Every signal assignment in a clocked process always
+     -----produces a flip flop and in vhdl specifically produces D-flip flop.
+
+     ------But this code will have one flip flop since the multiple signal 
+     ------assignment is applied to one output signal.
 
             elsif rising_edge(clock) then
                 if sig_x = '1' then
